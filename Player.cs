cc4ed81ccc1d241d99586d387e5cc712465953ee
scipiox64 <55637992+scipiox64@users.noreply.github.com>
2019-11-02 21:43:08 -0700

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public Text healthText;
+    
+    [SerializeField]
+    int _health;
+    
+    public Text poisonDurationText;
+    bool _isPoisoned;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _health = 100;
+        //healthText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "Health: " + _health.ToString();
+    }
+    public void IsPoisoned()
+    {
+        StartCoroutine(PoisonedTime());
+        StartCoroutine(PoisonHealth());
+    }
+    IEnumerator PoisonedTime()
+    {
+        _isPoisoned = true;
+        Debug.Log("_isPoisoned set to true");
+
+        if (_isPoisoned == true)
+        {
+            int randomPoisonDuration = Random.Range(5, 21);
+            poisonDurationText.text = "Poison Duration: " + randomPoisonDuration.ToString();
+            Debug.Log("Poisoned for: " + randomPoisonDuration + " seconds..");
+            yield return new WaitForSeconds(randomPoisonDuration);
+
+            _isPoisoned = default;
+            Debug.Log("_isPoisoned set to default");
+            Debug.Log("Subtracted: " + randomPoisonDuration + " health");
+        }
+    }
+
+    IEnumerator PoisonHealth()
+    {
+        while (_isPoisoned == true)
+        {
+            _health--;
+            Debug.Log("Health - 1, Waiting for 1 second.."); 
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+
+}
---
+++ PoisonBolt.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonBolt : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(1.009f, 0.5f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, 0.74f, 3.5f), transform.position.y, transform.position.z);
+
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Poison Bolt Hit Player! Player is Poisoned!");
+            transform.position = new Vector3(1.5f, 0.5f, 0);
+            Debug.Log("Poison Bolt Position Reset");
+            Player player = other.transform.GetComponent<Player>();
+            player.IsPoisoned();
+        }
+        
+    }
+
+
+
+
+
+}
